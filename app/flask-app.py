3c3c72ab1bee378b2c4d6bb39efaf3868b371261
ShanKonduru <ShanKonduru@gmail.com>
2023-10-04 15:32:39 +0530

    added html page

--- app/flask-app.py
+++ app/flask-app.py
@@ -1,11 +1,26 @@
 from flask import Flask
 import socket
 
+html_page = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Flask Application</title>
+</head>
+<body>
+    <h1>Hello, this is my Flask application.</h1>
+    <p>Serving you from: <strong>{hostname}</strong></p>
+</body>
+</html>
+"""
+
 app = Flask(__name__)
 
 @app.route("/")
 def  home():
-    return "Hello, this is my flask application.\r\nServing you from " + socket.gethostname()
+    # Replace {hostname} with the actual hostname
+    html_page = html_page.format(hostname=socket.gethostname())
+    return html_page;
 
 if __name__ == "__main__":
     app.run(debug=True)
