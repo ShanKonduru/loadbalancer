e9f5956fb680a4befd42dc9249cfec7a450ffb40
ShanKonduru <ShanKonduru@gmail.com>
2023-10-04 15:54:59 +0530

    fixed html with formatting changed

--- app/flask-app.py
+++ app/flask-app.py
@@ -1,25 +1,26 @@
 from flask import Flask
 import socket
 
-html_page = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Flask Application</title>
-</head>
-<body>
-    <h1>Hello, this is my Flask application.</h1>
-    <p>Serving you from: <strong>{hostname}</strong></p>
-</body>
-</html>
-"""
-
 app = Flask(__name__)
 
 @app.route("/")
 def  home():
-    # Replace {hostname} with the actual hostname
-    html_page = html_page.format(hostname=socket.gethostname())
+    # Get the hostname
+    hostname = socket.gethostname()
+
+    # Define the HTML template
+    html_page = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Flask Application</title>
+    </head>
+    <body>
+        <h1>Hello, this is my Flask application.</h1>
+        <p>Serving you from: <strong>{hostname}</strong></p>
+    </body>
+    </html>
+    """
     return html_page;
 
 if __name__ == "__main__":
